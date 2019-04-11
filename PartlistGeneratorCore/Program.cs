1ba409e115bca62195b16831694d28fc48e716c0
Mateusz <35574738+dolasinski24504@users.noreply.github.com>
2019-04-11 22:59:57 +0200

    No dictionary - working fine now
    Now iterating by positions of primal partlist, than checking with choosen parts numbers - if both contains adding to list then exporting.

--- PartlistGeneratorCore/Program.cs
+++ PartlistGeneratorCore/Program.cs
@@ -7,85 +7,101 @@ namespace PartlistGeneratorCore
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Partlist Generator\n");
 
-            OldPartlistReader Reader = new OldPartlistReader();
-
+            OldPartlistLoader Reader = new OldPartlistLoader();
             List<string> readedPartlist = Reader.Read();
-
             Reader.PrintReadedPartlist(readedPartlist);
 
-            NewPartlistWritter Writter = new NewPartlistWritter();
+            PartListPreparer Preparer = new PartListPreparer();
+            Preparer.PreparePartlist(readedPartlist);
 
-            Writter.Write(readedPartlist);
+            List<string> finalPartlist = Preparer.PreparePartlist(readedPartlist);
+            NewPartlistExporter Exporter = new NewPartlistExporter();
+            Exporter.ExportToTXT(finalPartlist);
+        }
+    }
 
+    class OldPartlistLoader
+    {
+        public List<string> Read()
+        {
+            var partlistReadPathTXT = @"C:\Users\DolaX\Desktop\Sharp\Moje\PartlistGerator\PartlistGeneratorCore\Partlist.txt";
+            var partlistReadPathEXCEL = @"C:\Users\DolaX\Desktop\Sharp\Moje\PartlistGerator\PartlistGeneratorCore\Partlist.xlsx";
 
+            List<string> loadedPartlistTXT = File.ReadAllLines(partlistReadPathTXT).ToList();
+            List<string> loadedPartlistEXCEL = File.ReadAllLines(partlistReadPathEXCEL).ToList();
 
-
-            ////////////// 
-            ///TEST
-
-            Dictionary<string, string> prearedPartlist = new Dictionary<string, string>();
-
-            string[] splittedList = new string[4];
-
-            Console.WriteLine("\n");
-            foreach (var item in readedPartlist)
-            {
-                splittedList = item.Split('\t');
-                Console.WriteLine($"{splittedList[0]} - {splittedList[2]} - {splittedList[3]} ");
-
-                prearedPartlist.Add(Convert.ToString(item[0]), Convert.ToString(item[2]));
-
-            }
-
-
-            ////TEST END
-            //////////////
-
+            return loadedPartlistTXT;
         }
 
-
-        class OldPartlistReader
+        public void PrintReadedPartlist(List<string> Partlist)
         {
-            public List<string> Read()
+            foreach (var line in Partlist)
             {
-                var partlistReadPathTXT = @"C:\Users\DolaX\Desktop\Sharp\Moje\PartlistGerator\PartlistGeneratorCore\TESTPartlist.txt";
-                var partlistReadPathEXCEL = @"C:\Users\DolaX\Desktop\Sharp\Moje\PartlistGerator\PartlistGeneratorCore\Partlist.xlsx";
-
-                List<string> loadedPartlistTXT = File.ReadAllLines(partlistReadPathTXT).ToList();
-                List<string> loadedPartlistEXCEL = File.ReadAllLines(partlistReadPathEXCEL).ToList();
-
-                return loadedPartlistTXT;
-            }
-
-            public void PrintReadedPartlist(List<string> Partlist)
-            {
-                foreach (var line in Partlist)
-                {
-                    Console.WriteLine(line);
-                }
-            }
-
-        }
-
-        class NewPartlistWritter
-        {
-            public void Write(List<string> finalPartlist)
-            {
-                var partlistWritePath = @"C:\Users\DolaX\Desktop\Sharp\Moje\PartlistGerator\PartlistGeneratorCore\NewPartlist.txt";
-                File.WriteAllLines(partlistWritePath, finalPartlist);
-            }
-        }
-
-        class TextConverter
-        {
-            public void Convert()
-            {
-
+                Console.WriteLine(line);
             }
         }
     }
+
+    class NewPartlistExporter
+    {
+        public void ExportToTXT(List<string> finalPartlist)
+        {
+            var partlistWritePath = @"C:\Users\DolaX\Desktop\Sharp\Moje\PartlistGerator\PartlistGeneratorCore\NewPartlist.txt";
+            File.WriteAllLines(partlistWritePath, finalPartlist);
+        }
+    }
+
+    class PartListPreparer
+    {
+        public List<string> PreparePartlist(List<string> readedPartlist)
+        {
+            List<string> choosenParts = new List<string>();
+            choosenParts.Add("Pos\tBOM Component\tQuantity");
+
+            List<string> allPartNumbers = new List<string>();
+
+            List<string> choosenPartNumber = new List<string>();
+
+            choosenPartNumber.Add("031");
+            choosenPartNumber.Add("001");
+            choosenPartNumber.Add("020");
+            choosenPartNumber.Add("061");
+            choosenPartNumber.Add("101");
+            choosenPartNumber.Add("160");
+            choosenPartNumber.Add("320");
+            choosenPartNumber.Add("305");
+            choosenPartNumber.Add("301");
+            choosenPartNumber.Add("1200");
+            choosenPartNumber.Add("666");
+
+            string[] splittedList = new string[4];
+
+            foreach (var item in readedPartlist)
+            {
+                splittedList = item.Split('\t');
+
+                if (choosenPartNumber.Contains(splittedList[0]))
+                {
+                    choosenParts.Add($"{splittedList[0]}\t{splittedList[2]}\t{splittedList[3]}");
+                    allPartNumbers.Add($"{splittedList[0]}");
+                }
+            }
+
+            foreach (var item in choosenPartNumber)
+            {
+                if (!allPartNumbers.Contains(item))
+                {
+                    choosenParts.Add($"{item} is not on primal partlist!!!");
+                }
+            }
+
+            return choosenParts;
+        }
+    }
 }
+
